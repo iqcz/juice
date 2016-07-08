1cf3a4438adc6d48abd6f08fdd5e57293e54003f
iqcz <a572511quchengzhi@gmail.com>
2016-07-08 15:29:31 +0800

    format code.

--- jhtp/jdk8/src/com/lee/how/to/program/chapter17/ProcessingEmployees.java
+++ jhtp/jdk8/src/com/lee/how/to/program/chapter17/ProcessingEmployees.java
@@ -16,13 +16,13 @@ public class ProcessingEmployees {
 	public static void main(String[] args) {
 		// initialize array of Employees
 		Employee[] employees = { 
-				new Employee("Jason", "Red", 5000, "IT"), 
-				new Employee("Ashley", "Green", 7600, "IT"),
-				new Employee("Matthew", "Indigo", 3587.5, "Sales"),
-				new Employee("James", "Indigo", 4700.77, "Marketing"), 
-				new Employee("Luke", "Indigo", 6200, "IT"),
-				new Employee("Jason", "Blue", 3200, "Sales"), 
-				new Employee("Wendy", "Brown", 4236.4, "Marketing") 
+				new Employee("Jason",   "Red",     5000,    "IT"), 
+				new Employee("Ashley",  "Green",   7600,    "IT"),
+				new Employee("Matthew", "Indigo",  3587.5,  "Sales"),
+				new Employee("James",   "Indigo",  4700.77, "Marketing"), 
+				new Employee("Luke",    "Indigo",  6200,    "IT"),
+				new Employee("Jason",   "Blue",    3200,    "Sales"), 
+				new Employee("Wendy",   "Brown",   4236.4,  "Marketing") 
 		};
 
 		// get List view of the Employees
