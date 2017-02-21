2a71f06be0fca874f68bd4e826f03d6e62ded559
TurtusLi <a2z0to9@outlook.com>
2017-02-22 07:49:54 +0800

    change comment.

--- hello-prpject/src/main/java/com/packt/mvneclipse/javainanutshell/ManInfo.java
+++ hello-prpject/src/main/java/com/packt/mvneclipse/javainanutshell/ManInfo.java
@@ -1,7 +1,8 @@
 package com.packt.mvneclipse.javainanutshell;
 
 /*
-class叫做“类字面量”，因class是关键字, 所以class编译时确定，getclass（）运行时根据实际实例确定。
+class叫做“类字面量”，因class是关键字, 所以class编译时确定，
+getclass（）运行时根据实际实例确定。
 String.class 是能对类名的引用取得在内存中该类型class对象的引用， 
 new String().getClass() 是通过实例对象取得在内存中该实际类型class对象的引用 
 （这个方法是从java.lang.Object类继承过来的）
