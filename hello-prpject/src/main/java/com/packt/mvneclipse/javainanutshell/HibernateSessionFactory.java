e915d3257548751d35dffa08234acf2722c83e1c
iqcz <a572511quchengzhi@gmail.com>
2017-03-06 23:16:45 +0800

    comment the error code.

--- hello-prpject/src/main/java/com/packt/mvneclipse/javainanutshell/HibernateSessionFactory.java
+++ hello-prpject/src/main/java/com/packt/mvneclipse/javainanutshell/HibernateSessionFactory.java
@@ -1,8 +1,8 @@
 package com.packt.mvneclipse.javainanutshell;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.cfg.Configuration;
+//import org.hibernate.HibernateException;
+//import org.hibernate.Session;
+//import org.hibernate.cfg.Configuration;
 
 /**
  * Configures and provides access to Hibernate sessions, tied to the current thread of execution. Follows the Thread
@@ -22,13 +22,13 @@ public class HibernateSessionFactory {
      private static String CONFIG_FILE_LOCATION = "/hibernate.cfg.xml";
 
      /** Holds a single instance of Session */
-     private static final ThreadLocal<Session> threadLocal = new ThreadLocal<>();
-
-     /** The single instance of hibernate configuration */
-     private static final Configuration cfg = new Configuration();
-
-     /** The single instance of hibernate SessionFactory */
-     private static org.hibernate.SessionFactory sessionFactory;
+//     private static final ThreadLocal<Session> threadLocal = new ThreadLocal<>();
+//
+//     /** The single instance of hibernate configuration */
+//     private static final Configuration cfg = new Configuration();
+//
+//     /** The single instance of hibernate SessionFactory */
+//     private static org.hibernate.SessionFactory sessionFactory;
 
      /**
       * Default constructor.
@@ -42,38 +42,38 @@ public class HibernateSessionFactory {
       * @return Session
       * @throws HibernateException
       */
-     public static Session currentSession() throws HibernateException {
-          Session session = (Session) threadLocal.get();
-
-          if (session == null) {
-               if (sessionFactory == null) {
-                    try {
-                         cfg.configure(CONFIG_FILE_LOCATION);
-                         sessionFactory = cfg.buildSessionFactory();
-                    } catch (Exception e) {
-                         System.err.println("%%%% Error Creating SessionFactory %%%%");
-                         e.printStackTrace();
-                    }
-               }
-               session = sessionFactory.openSession();
-               threadLocal.set(session);
-          }
-
-          return session;
-     }
+//     public static Session currentSession() throws HibernateException {
+//          Session session = (Session) threadLocal.get();
+//
+//          if (session == null) {
+//               if (sessionFactory == null) {
+//                    try {
+//                         cfg.configure(CONFIG_FILE_LOCATION);
+//                         sessionFactory = cfg.buildSessionFactory();
+//                    } catch (Exception e) {
+//                         System.err.println("%%%% Error Creating SessionFactory %%%%");
+//                         e.printStackTrace();
+//                    }
+//               }
+//               session = sessionFactory.openSession();
+//               threadLocal.set(session);
+//          }
+//
+//          return session;
+//     }
 
      /**
       * Close the single hibernate session instance.
       *
       * @throws HibernateException
       */
-     public static void closeSession() throws HibernateException {
-          Session session = threadLocal.get();
-          threadLocal.set(null);
-
-          if (session != null) {
-               session.close();
-          }
-     }
+//     public static void closeSession() throws HibernateException {
+//          Session session = threadLocal.get();
+//          threadLocal.set(null);
+//
+//          if (session != null) {
+//               session.close();
+//          }
+//     }
 
 }
