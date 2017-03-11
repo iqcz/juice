a0858b1d0b507507767565006df6c42747a039a9
TurtusLi <a2z0to9@outlook.com>
2017-03-11 19:52:30 +0800

    The value of @Repeatable indicates the containing annotation type for the repeatable annotation type.

---
+++ hello-prpject/src/main/java/com/packt/java8inaction/repeatableanno/Authors.java
@@ -0,0 +1,11 @@
+package com.packt.java8inaction.repeatableanno;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authors {
+
+    Author[] value();
+
+}
