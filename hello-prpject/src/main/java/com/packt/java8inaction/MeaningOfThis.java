d60750b5656db341864defff1ea40dc510f2bb40
iqcz <a572511quchengzhi@gmail.com>
2017-03-21 04:15:13 +0800

    What is the final value for printing this?

---
+++ hello-prpject/src/main/java/com/packt/java8inaction/MeaningOfThis.java
@@ -0,0 +1,32 @@
+package com.packt.java8inaction;
+
+/**
+ * What is the final value for printing this?
+ * @author i324779
+ *
+ */
+public class MeaningOfThis {
+
+    public final int value = 4;
+
+    public void doInit() {
+	int value = 6;
+	Runnable r = new Runnable() {
+	    public final int value = 5;
+
+	    @Override
+	    public void run() {
+		int value = 10;
+		System.out.println(this.value);
+		System.out.println(value);
+	    }
+	};
+	r.run();
+    }
+
+    public static void main(String[] args) {
+	MeaningOfThis m = new MeaningOfThis();
+	m.doInit();
+    }
+
+}
