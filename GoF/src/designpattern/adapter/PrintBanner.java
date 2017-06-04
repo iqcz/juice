7b9aaff0651c26e4b65e9774bb6aa020da9b78b3
TurtusLi <a2z0to9@outlook.com>
2017-06-04 21:34:24 +0800

    使用了继承的适配器模式

---
+++ GoF/src/designpattern/adapter/PrintBanner.java
@@ -0,0 +1,22 @@
+package designpattern.adapter;
+
+/**
+ * 适配器类
+ * @author TurtusLi
+ *
+ */
+public class PrintBanner extends Banner implements Print {
+    
+    public PrintBanner(String string) {
+        super(string);
+    }
+    
+    public void printWeak() {
+        showWithParen();
+    }
+    
+    public void printStrong() {
+        showWithAster();
+    }
+    
+}
