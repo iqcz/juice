965cc5d7f03abaad8b6129a3bb2fece691e70d5e
TurtusLi <a2z0to9@outlook.com>
2017-06-29 23:39:25 +0800

    定义并访问内部类的方式

---
+++ hello-prpject/src/main/java/exam/Outter.java
@@ -0,0 +1,28 @@
+package exam;
+
+/**
+ * 定义并访问内部类的方式
+ * @author TurtusLi
+ *
+ */
+public class Outter {
+
+    String str = "Outter";
+    class Inner {
+	int i = 9;
+	
+	public String sayHello() {
+	    return str;
+	}
+    }
+    public static void main(String[] args) {
+	Outter o = new Outter();
+	Outter.Inner inner = o.new Inner();
+	
+	System.out.println(inner.i);
+	System.out.println(inner.sayHello());
+	
+//	Outter.Inner inner2 = new Outter().new Inner();
+    }
+
+}
