ed2a730a0585b4e03e28233e04c4b69d8a23a867
TurtusLi <a2z0to9@outlook.com>
2017-07-11 23:38:44 +0800

    null 的static调用。

---
+++ hello-prpject/src/main/java/exam/Example015.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Example015 {
+
+    public static void hello() {
+	System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+	((Example015) null).hello();
+    }
+
+}
