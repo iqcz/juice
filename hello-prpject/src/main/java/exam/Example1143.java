c58d2e803818298410943633d7d1aa957c214f24
TurtusLi <a2z0to9@outlook.com>
2017-07-18 19:30:54 +0800

    如何捕获一个线程抛出的异常？

---
+++ hello-prpject/src/main/java/exam/Example1143.java
@@ -0,0 +1,30 @@
+package exam;
+
+/**
+ * 如何捕获一个线程抛出的异常？
+ * @author TurtusLi
+ *
+ */
+public class Example1143 {
+
+    // 处理异常的handler
+    private static Thread.UncaughtExceptionHandler handler = (Thread t, Throwable e) -> {
+	System.out.println("Uncaught exception:" + e);
+    };
+
+    public static void main(String[] args) {
+	Thread myThread = new MyThread();
+	//设置捕获异常的handler
+	myThread.setUncaughtExceptionHandler(handler);
+	myThread.start();
+    }
+
+}
+
+class MyThread extends Thread {
+
+    public void run() {
+	System.out.println("Thread will throw exception.");
+	throw new RuntimeException("My own exception from thread.");
+    }
+}
