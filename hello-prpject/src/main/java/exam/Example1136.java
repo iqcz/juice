9d75d4fffbae92c63277990d957b05cd644e465c
TurtusLi <a2z0to9@outlook.com>
2017-07-20 23:47:34 +0800

    对finally的使用，添加注释。

--- hello-prpject/src/main/java/exam/Example1136.java
+++ hello-prpject/src/main/java/exam/Example1136.java
@@ -1,5 +1,13 @@
 package exam;
 
+/**
+ * 关于finally是否执行的例子。
+ * 正常情况下，finally都会执行，而且是在返回return之前执行，
+ * 但是遇到System.exit(1)还是System.exit(0)，
+ * 也就是不管程序是否正常推出，都不再执行final有里的语句了。
+ * @author TurtusLi
+ *
+ */
 public class Example1136 {
 
     public static void main(String[] args) {
