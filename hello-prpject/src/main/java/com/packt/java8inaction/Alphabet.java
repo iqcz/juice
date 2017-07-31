2cff07a722748ea6b13daae9dc7d0fa17ce37d0f
TurtusLi <a2z0to9@outlook.com>
2017-07-31 22:48:34 +0800

    default method in interface

---
+++ hello-prpject/src/main/java/com/packt/java8inaction/Alphabet.java
@@ -0,0 +1,22 @@
+package com.packt.java8inaction;
+
+public interface Alphabet {
+
+    default boolean isAtOddPos(char c) {
+	if (!Character.isLetter(c)) {
+	    throw new RuntimeException("Not a letter: " + c);
+	}
+	char uc = Character.toUpperCase(c);
+	int pos = uc - 64;
+	return pos % 2 == 1;
+    }
+
+    default boolean isAtEvenPos(char c) {
+	if (!Character.isLetter(c)) {
+	    throw new RuntimeException("Not a letter: " + c);
+	}
+	char uc = Character.toUpperCase(c);
+	int pos = uc - 64;
+	return pos % 2 == 0;
+    }
+}
