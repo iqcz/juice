ae3474a47e8fb658720164b9e9ed0f3189c601c8
Felix <a572511quchengzhi@gmail.com>
2017-08-17 21:34:29 +0800

    JDK  source code
    see code

---
+++ hello-prpject/src/main/java/exam/ForInterview.java
@@ -0,0 +1,30 @@
+package exam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 在JDK 8 下，查看这些类的源代码
+ * @author phideon
+ *
+ */
+public class ForInterview {
+
+	public static void main(String[] args) {
+
+		List<String> names = new ArrayList<>();
+		
+		Set<String> schools = new HashSet<>();
+		
+		Map<Integer, String> map = new HashMap<>();
+		
+		Map<Integer, String> conMap = new ConcurrentHashMap<>();
+		
+	}
+
+}
