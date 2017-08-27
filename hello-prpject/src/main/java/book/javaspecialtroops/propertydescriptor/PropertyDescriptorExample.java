2fcf58f7ca88b9b1504d0e4c58027d9094be8664
Felix <a572511quchengzhi@gmail.com>
2017-08-27 10:52:35 +0800

    PropertyDescriptor usage.

---
+++ hello-prpject/src/main/java/book/javaspecialtroops/propertydescriptor/PropertyDescriptorExample.java
@@ -0,0 +1,35 @@
+package book.javaspecialtroops.propertydescriptor;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+
+/**
+ * PropertyDescriptor usage.
+ * @author phideon
+ *
+ */
+public class PropertyDescriptorExample {
+
+	public static void main(String[] args) throws IntrospectionException {
+		Field[] fields = Node.class.getDeclaredFields();
+
+		for (Field field : fields) {
+			System.out.println("========>" + field.getName() + "\t" + field.getType());
+
+			PropertyDescriptor pd = new PropertyDescriptor(field.getName(), Node.class);
+
+			printDesc(pd);
+
+		}
+	}
+
+	private static void printDesc(PropertyDescriptor pd) {
+		System.out.println(pd.getDisplayName());
+		System.out.println(pd.getShortDescription());
+		System.out.println(pd.getPropertyEditorClass());
+		System.out.println(pd.getPropertyType());
+		System.out.println(pd.getReadMethod());
+		System.out.println(pd.getWriteMethod());
+	}
+}
