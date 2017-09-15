d079d0760fabf7b863b1ba903ec24e91f47c7dd0
phideon <a0z0to9@outlook.com>
2017-09-15 23:18:33 +0800

    format the code.

--- hello-prpject/src/main/java/com/howtoprogram/ArraysAndStreams2.java
+++ hello-prpject/src/main/java/com/howtoprogram/ArraysAndStreams2.java
@@ -13,16 +13,22 @@ public class ArraysAndStreams2 {
 
 	// strings in upper case
 	System.out.printf("strings in uppercase: %s%n",
-		Arrays.stream(strings).map(String::toUpperCase).collect(Collectors.toList()));
+		Arrays.stream(strings)
+		.map(String::toUpperCase)
+		.collect(Collectors.toList()));
 
 	// strings less than "n" (case insensitive) sorted ascending
 	System.out.printf("strings greater than m sorted ascending: %s%n",
-		Arrays.stream(strings).filter(s -> s.compareToIgnoreCase("n") < 0).sorted(String.CASE_INSENSITIVE_ORDER)
-			.collect(Collectors.toList()));
+		Arrays.stream(strings)
+		.filter(s -> s.compareToIgnoreCase("n") < 0)
+		.sorted(String.CASE_INSENSITIVE_ORDER)
+		.collect(Collectors.toList()));
 
 	// strings less than "n" (case insensitive) sorted descending
 	System.out.printf("strings greater than m sorted descending: %s%n",
-		Arrays.stream(strings).filter(s -> s.compareToIgnoreCase("n") < 0)
-			.sorted(String.CASE_INSENSITIVE_ORDER.reversed()).collect(Collectors.toList()));
+		Arrays.stream(strings)
+		.filter(s -> s.compareToIgnoreCase("n") < 0)
+		.sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+		.collect(Collectors.toList()));
     }
 } // end class ArraysAndStreams2
