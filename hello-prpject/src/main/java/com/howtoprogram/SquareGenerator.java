80c9f57e2ef7efe68c3aca1b5a1ca628dd4e1b89
phideon <a0z0to9@outlook.com>
2017-10-02 23:47:54 +0800

    IntSupplier usage，可以在一次循环后，下一个循环的值接着上面循环的值。

---
+++ hello-prpject/src/main/java/com/howtoprogram/SquareGenerator.java
@@ -0,0 +1,41 @@
+package com.howtoprogram;
+
+import java.util.PrimitiveIterator;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
+
+/**
+ * 看运行结果
+ * @author i324779
+ *
+ */
+public class SquareGenerator implements IntSupplier {
+
+    private int current = 1;
+
+    @Override
+    public synchronized int getAsInt() {
+	int thisCurrent = current * current;
+	current++;
+
+	return thisCurrent;
+    }
+
+    public static void main(String[] args) {
+	IntStream squars = IntStream.generate(new SquareGenerator());
+
+	PrimitiveIterator.OfInt stepThrough = squars.iterator();
+
+	for (int i = 0; i < 10; i++) {
+	    System.out.println(stepThrough.nextInt());
+	}
+
+	System.out.println("First iterator done...");
+
+	// We can go on as long as we like...
+	for (int i = 0; i < 10; i++) {
+	    System.out.println(stepThrough.nextInt());
+	}
+    }
+
+}
