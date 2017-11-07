b19e8f4ea8f4556b9164edf4780959339fbba54e
phideon <a0z0to9@outlook.com>
2017-11-07 23:39:57 +0800

    format code

--- hello-prpject/src/main/java/com/howtoprogram/StringCompare.java
+++ hello-prpject/src/main/java/com/howtoprogram/StringCompare.java
@@ -2,25 +2,25 @@ package com.howtoprogram;
 
 public class StringCompare {
 
-	public static void main(String[] args) {
-		String s1 = new String("hello"); // s1 is a copy of "hello"
-		String s2 = "goodbye";
-		String s3 = "Happy Birthday";
-		String s4 = "happy birthday";
+    public static void main(String[] args) {
+	String s1 = new String("hello"); // s1 is a copy of "hello"
+	String s2 = "goodbye";
+	String s3 = "Happy Birthday";
+	String s4 = "happy birthday";
 
-		// test regionMatches (case sensitive)
-		if (s3.regionMatches(0, s4, 0, 5)) {
-			System.out.println("First 5 characters of s3 and s4 match");
-		} else {
-			System.out.println("First 5 characters of s3 and s4 do not match");
-		}
+	// test regionMatches (case sensitive)
+	if (s3.regionMatches(0, s4, 0, 5)) {
+	    System.out.println("First 5 characters of s3 and s4 match");
+	} else {
+	    System.out.println("First 5 characters of s3 and s4 do not match");
+	}
 
-		// test regionMatches (ignore case)
-		if (s3.regionMatches(true, 0, s4, 0, 5)) { // true means ignore string case.
-			System.out.println("First 5 characters of s3 and s4 match with case ignored");
-		} else {
-			System.out.println("First 5 characters of s3 and s4 do not match");
-		}
-		
-	} // end method main.
+	// test regionMatches (ignore case)
+	if (s3.regionMatches(true, 0, s4, 0, 5)) { // true means ignore string case.
+	    System.out.println("First 5 characters of s3 and s4 match with case ignored");
+	} else {
+	    System.out.println("First 5 characters of s3 and s4 do not match");
+	}
+
+    } // end method main.
 }
