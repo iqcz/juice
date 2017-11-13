0747045b16efd5029e8df8cfb24cd976cde91105
LiYanxue <liyanxue@acfun.tv>
2017-11-13 09:41:12 +0800

    顺序查找，效率最低的一种查找算法

---
+++ hello-prpject/src/main/java/searchalgorithm/LinearSearchTest.java
@@ -0,0 +1,61 @@
+package searchalgorithm;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Sequentially searching an array for an item. 顺序查找，效率最低的一种查找算法。
+ * 
+ * @author 324779
+ *
+ */
+public class LinearSearchTest {
+
+	/**
+	 * perform a linear search on the data
+	 * 
+	 * @param data
+	 *            数据
+	 * @param searchKey
+	 *            要查找的元素
+	 * @return
+	 */
+	public static int linearSearch(int data[], int searchKey) {
+		// loop through array sequentially
+		for (int index = 0; index < data.length; index++)
+			if (data[index] == searchKey)
+				return index; // return index of integer
+
+		return -1; // integer was not found
+	} // end method linearSearch
+
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			SecureRandom generator = new SecureRandom();
+			int[] data = new int[10]; // create array
+			for (int i = 0; i < data.length; i++) // populate array
+				data[i] = 10 + generator.nextInt(90);
+			
+			System.out.printf("%s%n%n", Arrays.toString(data)); // display array
+
+			// get input from user
+			System.out.print("Please enter an integer value (-1 to quit): ");
+			int searchInt = input.nextInt();
+
+			// repeatedly input an integer; -1 terminates the program
+			while (searchInt != -1) {
+				int position = linearSearch(data, searchInt); // perform search
+
+				if (position == -1) // not found
+					System.out.printf("%d was not found%n%n", searchInt);
+				else // found
+					System.out.printf("%d was found in position %d%n%n", searchInt, position);
+
+				// get input from user
+				System.out.print("Please enter an integer value (-1 to quit): ");
+				searchInt = input.nextInt();
+			}
+		}
+	} // end main
+} // end class LinearSearchTest
