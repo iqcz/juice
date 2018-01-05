bafa36a879b3e994caef946b4f9f715d4c5e5358
phideon <a0z0to9@outlook.com>
2018-01-05 23:26:31 +0800

    addAll，Comprable

---
+++ hello-prpject/src/main/java/com/howtoprogram/WordList.java
@@ -0,0 +1,17 @@
+package com.howtoprogram;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class WordList {
+    public static void main(String[] args) {
+	String[] names = { "zhangsan", "lisi", "lisi", "wangwu", "zhaoliu", "ali", "bili"};
+
+	// remove duplicated name in the array to add TreeSet.
+	// and ordered by alphabetical order for String class
+	Set<String> s = new TreeSet<>();
+	Collections.addAll(s, names);
+	System.out.println(s);
+    }
+}
