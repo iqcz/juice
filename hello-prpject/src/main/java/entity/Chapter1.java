405cc39b83221c443ee12637a183aecc2783b162
phideon <a0z0to9@outlook.com>
2018-01-08 22:59:04 +0800

    添加注释

--- hello-prpject/src/main/java/entity/Chapter1.java
+++ hello-prpject/src/main/java/entity/Chapter1.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package entity;
 
 import java.util.List;
@@ -14,21 +9,33 @@ import java.util.stream.Collectors;
  */
 public class Chapter1 extends MusicChapter {
 
+    /**
+     * 使用lambda的方式获取名字列表
+     * @return
+     */
     public List<String> getNamesOfArtists_Lambda() {
 	return artists.stream()
 		.map(artist -> artist.getName())
 		.collect(Collectors.toList());
     }
 
+    /**
+     * 使用方法引用的方式获取名字列表
+     * @return
+     */
     public List<String> getNamesOfArtists_MethodReference() {
 	return artists.stream()
 		.map(Artist::getName)
 		.collect(Collectors.toList());
     }
 
+    /**
+    * filter 方法的使用
+    * @return
+    */
     public List<Artist> artistsLivingInLondon() {
-	return artists.stream()
-		.filter(artist -> "London".equals(artist.getNationality()))
+	return artists.stream().
+		filter(artist -> "London".equals(artist.getNationality()))
 		.collect(Collectors.toList());
     }
 
