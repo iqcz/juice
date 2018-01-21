6a783d2b5b1e83e2ba0d24266f4a012510777132
phideon <a0z0to9@outlook.com>
2018-01-21 21:07:13 +0800

    javac will pick the most specific type for you

---
+++ hello-prpject/src/main/java/entity/OverloadResolution.java
@@ -0,0 +1,27 @@
+package entity;
+
+/**
+ * It’s possible in Java to overload methods, 
+ * so you have multiple methods with the same name but different signatures. 
+ * This approach poses a problem for parameter-type inference 
+ * because it means that there are several types that could be inferred. 
+ * In these situations
+ * javac will pick the most specific type for you
+ * @author i324779
+ *
+ */
+public class OverloadResolution {
+
+    public static void main(String[] args) {
+	OverloadResolution or = new OverloadResolution();
+	or.overloadedMethod("abc");
+    }
+
+    private void overloadedMethod(Object o) {
+	System.out.print("Object");
+    }
+
+    private void overloadedMethod(String s) {
+	System.out.print("String");
+    }
+}
