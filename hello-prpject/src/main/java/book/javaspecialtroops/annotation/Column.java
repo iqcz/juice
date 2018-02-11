0dd491b2a638d50926994e3d8110d3af60434af1
phideon <a0z0to9@outlook.com>
2018-02-11 23:50:45 +0800

    add comment.

--- hello-prpject/src/main/java/book/javaspecialtroops/annotation/Column.java
+++ hello-prpject/src/main/java/book/javaspecialtroops/annotation/Column.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * stand for db table column name.
+ * @author i324779
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Column {
