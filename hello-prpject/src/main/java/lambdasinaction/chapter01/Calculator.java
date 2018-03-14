549c801696c45e1b159e8a3640955813d0ea63d0
phideon <a0z0to9@outlook.com>
2018-03-14 23:47:03 +0800

    upgrading to java 8

---
+++ hello-prpject/src/main/java/lambdasinaction/chapter01/Calculator.java
@@ -0,0 +1,18 @@
+package lambdasinaction.chapter01;
+
+@FunctionalInterface
+public interface Calculator {
+
+    double calculate(int a, int b);
+
+    public default int subtract(int a, int b) {
+	return a - b;
+    }
+
+    public default int add(int a, int b) {
+	return a + b;
+    }
+
+    @Override
+    public String toString();
+}
