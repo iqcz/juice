8f4947289202ab2d70386b2d6643f35ac8e38ac8
phideon <a0z0to9@outlook.com>
2018-03-19 23:40:00 +0800

    some Functions demo.

---
+++ hello-prpject/src/main/java/lambdasinaction/chapter01/UnaryOperatorDemo.java
@@ -0,0 +1,19 @@
+package lambdasinaction.chapter01;
+
+import java.util.function.UnaryOperator;
+
+/**
+ * The UnaryOperator interface is another specialization of Function 
+ * whose operand type is the same as the return type. 
+ * Its declaration is as follows.
+ * @author i324779
+ *
+ */
+public class UnaryOperatorDemo {
+
+    public static void main(String[] args) {
+	UnaryOperator<String> appendSomething = str -> str + "->end";
+
+	System.out.println(appendSomething.apply("Ready to "));
+    }
+}
