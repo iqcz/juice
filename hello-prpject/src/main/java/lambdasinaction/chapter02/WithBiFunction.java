f4090f9d76bb4738c906da88f4f515840b25027d
phideon <a0z0to9@outlook.com>
2018-03-21 21:14:24 +0800

    BiFunction demo

---
+++ hello-prpject/src/main/java/lambdasinaction/chapter02/WithBiFunction.java
@@ -0,0 +1,30 @@
+package lambdasinaction.chapter02;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * As a side note, note that the abstract method in StringListFormatter takes two parameters and returns a value. 
+ * This is a good candidate for a BiFunction. 
+ * The WithBiFunction class in Listing 2.3 is a rewrite of the MethodReferenceDemo1 class 
+ * that eliminates the StringListFormatter interface entirely. 
+ * The formatAndPrint method has also been modified to accept a BiFunction as its first parameter.
+
+
+ * @author i324779
+ *
+ */
+public class WithBiFunction {
+
+    public static void formatAndPrint(BiFunction<String, List<String>, String> formatter,
+	    String delimiter, List<String> list) {
+	String formatted = formatter.apply(delimiter, list);
+	System.out.println(formatted);
+    }
+
+    public static void main(String[] args) {
+	List<String> names = Arrays.asList("Don", "King", "Kong");
+	formatAndPrint(String::join, ", ", names);
+    }
+}
