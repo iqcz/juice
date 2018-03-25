9a56f22caa671e8adf7bef8075eb5d237684f047
phideon <a0z0to9@outlook.com>
2018-03-25 22:20:05 +0800

    reference method demo 2.

---
+++ hello-prpject/src/main/java/lambdasinaction/chapter02/MethodReferenceDemo2.java
@@ -0,0 +1,15 @@
+package lambdasinaction.chapter02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferenceDemo2 {
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("Apple", "Banana");
+        // with lambda expression
+        fruits.forEach((name) -> System.out.println(name));
+
+        // with method reference
+        fruits.forEach(System.out::println);
+    }
+}
