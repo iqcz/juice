94f073ff2c259e073be56edbdb943ad763631a23
phideon <a0z0to9@outlook.com>
2018-03-29 22:37:19 +0800

    Reference to A Constructor

---
+++ hello-prpject/src/main/java/lambdasinaction/chapter02/MethodReferenceDemo4.java
@@ -0,0 +1,42 @@
+package lambdasinaction.chapter02;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.function.Supplier;
+
+/**
+ * Need a method to convert an Integer array to a Collection 
+ * and you need to be able to determine 
+ * whether the resulting collection is a List or a Set. 
+ * To this end, you can create the arrayToCollection method.
+ * 
+ * @author i324779
+ *
+ */
+
+public class MethodReferenceDemo4 {
+
+    public static Collection<Integer> arrayToCollection(Supplier<Collection<Integer>> supplier, Integer[] numbers) {
+	Collection<Integer> collection = supplier.get();
+	
+	for (int i : numbers) {
+	    collection.add(i);
+	}
+	
+	return collection;
+    }
+
+    public static void main(String[] args) {
+	Integer[] array = { 1, 8, 5 };
+	Collection<Integer> col1 = arrayToCollection(ArrayList<Integer>::new, array);
+	System.out.println("Natural order");
+	col1.forEach(System.out::println);
+	
+	System.out.println("=======================");
+	
+	System.out.println("Ascending order");
+	Collection<Integer> col2 = arrayToCollection(HashSet<Integer>::new, array);
+	col2.forEach(System.out::println);
+    }
+}
