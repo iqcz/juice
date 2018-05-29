ff7a051218750bc885f668010e7801d6956c51da
Phideon <a0z0to9@outlook.com>
2018-05-29 21:12:34 +0800

    stream concat.

--- hello-prpject/src/main/java/com/howtoprogram/BigThanNumbersCount.java
+++ hello-prpject/src/main/java/com/howtoprogram/BigThanNumbersCount.java
@@ -1,6 +1,7 @@
 package com.howtoprogram;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 给定一个int类型的数组，计算大于指定元素的个数，实用IntStream来实现。
@@ -12,10 +13,12 @@ public class BigThanNumbersCount {
 
     public static void main(String[] args) {
 	int[] nums = { 2, 4, 5, 6, 7, 90, 122 };
-
 	long count = IntStream.of(nums).filter(e -> e > 5).count();
-	
 	System.out.println(count);
+
+	Stream<String> stream1 = Stream.of("January", "Christie");
+	Stream<String> stream2 = Stream.of("Okanagan", "Sydney", "Alpha");
+	Stream.concat(stream1, stream2).sorted().forEach(System.out::println);
     }
 
 }
