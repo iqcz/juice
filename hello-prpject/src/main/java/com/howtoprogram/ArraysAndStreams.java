6cea4cee1da14a82c7c652382711385210df5928
Phideon <a0z0to9@outlook.com>
2018-06-13 23:59:29 +0800

    format to see clearly.

--- hello-prpject/src/main/java/com/howtoprogram/ArraysAndStreams.java
+++ hello-prpject/src/main/java/com/howtoprogram/ArraysAndStreams.java
@@ -13,18 +13,27 @@ public class ArraysAndStreams {
 	System.out.printf("Original values: %s%n", Arrays.asList(values));
 
 	// sort values in ascending order with streams
-	System.out.printf("Sorted values: %s%n", Arrays.stream(values).sorted().collect(Collectors.toList()));
+	System.out.printf("Sorted values: %s%n", Arrays.stream(values)
+		.sorted()
+		.collect(Collectors.toList()));
 
 	// values greater than 4
-	List<Integer> greaterThan4 = Arrays.stream(values).filter(value -> value > 4).collect(Collectors.toList());
+	List<Integer> greaterThan4 = Arrays.stream(values)
+		.filter(value -> value > 4)
+		.collect(Collectors.toList());
 	System.out.printf("Values greater than 4: %s%n", greaterThan4);
 
 	// filter values greater than 4 then sort the results
 	System.out.printf("Sorted values greater than 4: %s%n",
-		Arrays.stream(values).filter(value -> value > 4).sorted().collect(Collectors.toList()));
+		Arrays.stream(values)
+		.filter(value -> value > 4)
+		.sorted()
+		.collect(Collectors.toList()));
 
 	// greaterThan4 List sorted with streams
 	System.out.printf("Values greater than 4 (ascending with streams): %s%n",
-		greaterThan4.stream().sorted().collect(Collectors.toList()));
+		greaterThan4.stream().
+		sorted()
+		.collect(Collectors.toList()));
     }
 } // end class ArraysAndStreams
