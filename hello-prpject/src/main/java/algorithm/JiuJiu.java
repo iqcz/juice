27ec34b6bc3d26cea835eeba9780f921e8bd86a4
yamao <thinkmore7@gmail.com>
2018-07-17 09:50:27 +0800

    jiujiu

---
+++ hello-prpject/src/main/java/algorithm/JiuJiu.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+/**
+ * 使用一层循环打印九九乘法表
+ * @author User
+ *
+ */
+public class JiuJiu {
+
+	public static void main(String[] args) {
+		for (int i = 1, j = 1; j <= 9; i++) {
+			System.out.print(i + " * " + j + " = " + i * j + "    ");
+			if (i == j) {
+				i = 0;
+				j++;
+				System.out.println();
+			}
+		}
+	}
+}
