c9c8e7c3657fe817f7e84c90e55020c0469d1480
Phideon <a0z0to9@outlook.com>
2018-08-01 23:22:35 +0800

    basic thread demo.

---
+++ hello-prpject/src/main/java/com/java7concurrency/c1/Main.java
@@ -0,0 +1,11 @@
+package com.java7concurrency.c1;
+
+public class Main {
+    public static void main(String... args) {
+        for (int i = 1; i <= 10; i++) {
+            Calculator calculator = new Calculator(i);
+            Thread thread = new Thread(calculator);
+            thread.start();
+        }
+    }
+}
