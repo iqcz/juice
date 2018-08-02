6656fb46c39842eb76423431e8582adc913c2d97
Phideon <a2z0to9@outlook.com>
2018-08-02 15:11:39 +0800

    Getting and setting thread information.

--- hello-prpject/src/main/java/com/java7concurrency/c2/Calculator.java
+++ hello-prpject/src/main/java/com/java7concurrency/c2/Calculator.java
@@ -1,8 +1,21 @@
 package com.java7concurrency.c2;
 
 /**
- * @author yourname
+ * @author i324779
  *         Created on 2018-08-02
  */
-public class Calculator {
+public class Calculator implements Runnable {
+    private int number;
+
+    public Calculator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%s: %d * %d = %d\n",
+                    Thread.currentThread().getName(), number, i, i * number);
+        }
+    }
 }
