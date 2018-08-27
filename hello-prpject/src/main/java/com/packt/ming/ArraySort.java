7e126c6acf1aa224fa50c8a36b9920db021743a0
YaMao <a2z0to9@outlook.com>
2018-08-27 23:47:58 +0800

    format and use method reference.

--- hello-prpject/src/main/java/com/packt/ming/ArraySort.java
+++ hello-prpject/src/main/java/com/packt/ming/ArraySort.java
@@ -1,32 +1,31 @@
 package com.packt.ming;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArraySort {
 
     public static void main(String[] args) {
-		Integer[] numbers = { 2, 9, 90, 1, 92 };
-	//	Arrays.parallelSort(numbers, (first, second) -> first.compareTo(second));
-	//	Arrays.parallelSort(numbers, (first, second) -> {return first.compareTo(second);});
-		
-		/*
-		for(Integer n : numbers) {
-		    System.out.printf("%d, ", n);
-		}
-		
-		Runnable run1 = () -> System.out.println("Thread is running.");
-		*/
-		
-		// 使用代码快初始化 list 的 value
-		List<String> extensions = new ArrayList<String>() {
-			private static final long serialVersionUID = 2028514244500563526L;
-			{
-				add("action");
-				add("");
-			}
-		};
-		
-		System.out.println(extensions);
+        Integer[] numbers = { 2, 9, 90, 1, 92 };
+        // Arrays.parallelSort(numbers, (first, second) -> first.compareTo(second));
+        Arrays.parallelSort(numbers, Integer::compareTo);
+
+        for (Integer n : numbers) {
+            System.out.printf("%d, ", n);
+        }
+
+        Runnable run1 = () -> System.out.println("Thread is running.");
+
+        // 使用代码快初始化 list 的 value
+        List<String> extensions = new ArrayList<String>() {
+            private static final long serialVersionUID = 2028514244500563526L;
+            {
+                add("action");
+                add("");
+            }
+        };
+
+        System.out.println(extensions);
     }
 }
--- hello-prpject/src/main/java/com/packt/mvneclipse/guava/CharMatcherExample.java
+++ hello-prpject/src/main/java/com/packt/mvneclipse/guava/CharMatcherExample.java
@@ -4,34 +4,33 @@ import com.google.common.base.CharMatcher;
 
 public class CharMatcherExample {
 
-	public static void main(String[] args) {
-		 String stringWithLinebreaks = "This is an example\n"+
-                 "of a String with linebreaks\n"+
-                 "we want on one line";
-		 
-		 // replaceFrom 只是替换一个空格或换行，或tab等
-		 String str1 = CharMatcher.BREAKING_WHITESPACE.replaceFrom(stringWithLinebreaks, " ");
-		 System.out.println(str1);
-		 
-		 System.out.println("--------------------");
-		 
-		 // collapseFrom 替换多个空格或tab。
-		 String tabsAndSpaces = "String  with      spaces     and           tabs";
-		 String str2 = CharMatcher.WHITESPACE.collapseFrom(tabsAndSpaces, '|');
-		 System.out.println(str2);
-		 
-		 System.out.println("--------------------");
-		 
-		 // 只保留数字的字符串
-		 String lettersAndNumbers = "foo989yxbar234";
-		 String str3 = CharMatcher.JAVA_DIGIT.retainFrom(lettersAndNumbers);
-		 System.out.println(str3);
-		 
-		 System.out.println("--------------------");
-		 
-		 CharMatcher cm = CharMatcher.inRange('A', 'N');
-		 String s4 = cm.retainFrom("aaaABbbTEccCdN!4ddDEeee");
-		 System.out.println(s4);
-	}
+    public static void main(String[] args) {
+        String stringWithLinebreaks = "This is an example\n" + "of a String with linebreaks\n"
+                + "we want on one line";
+
+        // replaceFrom 只是替换一个空格或换行，或tab等
+        String str1 = CharMatcher.BREAKING_WHITESPACE.replaceFrom(stringWithLinebreaks, " ");
+        System.out.println(str1);
+
+        System.out.println("--------------------");
+
+        // collapseFrom 替换多个空格或tab。
+        String tabsAndSpaces = "String  with      spaces     and           tabs";
+        String str2 = CharMatcher.WHITESPACE.collapseFrom(tabsAndSpaces, '|');
+        System.out.println(str2);
+
+        System.out.println("--------------------");
+
+        // 只保留数字的字符串
+        String lettersAndNumbers = "foo989yxbar234";
+        String str3 = CharMatcher.JAVA_DIGIT.retainFrom(lettersAndNumbers);
+        System.out.println(str3);
+
+        System.out.println("--------------------");
+
+        CharMatcher cm = CharMatcher.inRange('A', 'N');
+        String s4 = cm.retainFrom("aaaABbbTEccCdN!4ddDEeee");
+        System.out.println(s4);
+    }
 
 }
