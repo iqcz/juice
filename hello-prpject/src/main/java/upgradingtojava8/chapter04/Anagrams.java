e5936f00b0e7efe88065a23312b0897e10fdfaef
phideon <a0z0to9@outlook.com>
2018-09-09 22:11:25 +0800

    基于Stream的同位词算法。

---
+++ hello-prpject/src/main/java/upgradingtojava8/chapter04/Anagrams.java
@@ -0,0 +1,47 @@
+package upgradingtojava8.chapter04;
+
+// Overuse of streams - don't do this!
+
+import static java.util.stream.Collectors.groupingBy;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Anagrams {
+
+    public static void main(String[] args) throws IOException {
+        Path dictionary = Paths.get(args[0]);
+        int minGroupSize = Integer.parseInt(args[1]);
+
+        /* 过度使用Stream
+        try (Stream<String> words = Files.lines(dictionary)) {
+            words.collect(groupingBy(word -> word.chars().sorted()
+                    .collect(StringBuilder::new, (sb, c) -> sb.append((char) c),
+                            StringBuilder::append)
+                    .toString()))
+                    .values().stream().
+                    filter(group -> group.size() >= minGroupSize)
+                    .map(group -> group.size() + ": " + group)
+                    .forEach(System.out::println);
+        
+        }
+        */
+
+        try (Stream<String> words = Files.lines(dictionary)) {
+            words.collect(groupingBy(Anagrams::alphabetize)).values().stream()
+                    .filter(group -> group.size() >= minGroupSize)
+                    .forEach(g -> System.out.println(g.size() + ": " + g));
+
+        }
+    }
+
+    private static String alphabetize(String s) {
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
+    }
+}
