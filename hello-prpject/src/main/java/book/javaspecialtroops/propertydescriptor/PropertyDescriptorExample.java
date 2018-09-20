28b39a8c8270a074acfc7d86f4a63a8b07445962
phideon <a0z0to9@outlook.com>
2018-09-20 21:35:23 +0800

    clear up Code.

--- hello-prpject/src/main/java/book/javaspecialtroops/propertydescriptor/PropertyDescriptorExample.java
+++ hello-prpject/src/main/java/book/javaspecialtroops/propertydescriptor/PropertyDescriptorExample.java
@@ -11,22 +11,22 @@ import java.lang.reflect.Field;
  */
 public class PropertyDescriptorExample {
 
-	public static void main(String[] args) throws IntrospectionException {
-		Field[] fields = Node.class.getDeclaredFields();
+    public static void main(String[] args) throws IntrospectionException {
+        Field[] fields = Node.class.getDeclaredFields();
 
-		for (Field field : fields) {
-			System.out.println("========>" + field.getName() + "\t" + field.getType());
-			PropertyDescriptor pd = new PropertyDescriptor(field.getName(), Node.class);
-			printDesc(pd);
-		}
-	}
+        for (Field field : fields) {
+            System.out.println("========>" + field.getName() + "\t" + field.getType());
+            PropertyDescriptor pd = new PropertyDescriptor(field.getName(), Node.class);
+            printDesc(pd);
+        }
+    }
 
-	private static void printDesc(PropertyDescriptor pd) {
-		System.out.println(pd.getDisplayName());
-		System.out.println(pd.getShortDescription());
-		System.out.println(pd.getPropertyEditorClass());
-		System.out.println(pd.getPropertyType());
-		System.out.println(pd.getReadMethod());
-		System.out.println(pd.getWriteMethod());
-	}
+    private static void printDesc(PropertyDescriptor pd) {
+        System.out.println(pd.getDisplayName());
+        System.out.println(pd.getShortDescription());
+        System.out.println(pd.getPropertyEditorClass());
+        System.out.println(pd.getPropertyType());
+        System.out.println(pd.getReadMethod());
+        System.out.println(pd.getWriteMethod());
+    }
 }
