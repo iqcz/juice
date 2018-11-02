ae508c62f7df14e54e71dd7511183ea27eacd3d4
Phideon <a2z0to9@outlook.com>
2018-11-02 23:35:18 +0800

    change code

--- hello-prpject/src/main/java/concurrency/JoinThread.java
+++ hello-prpject/src/main/java/concurrency/JoinThread.java
@@ -7,30 +7,31 @@ package concurrency;
  */
 public class JoinThread extends Thread {
 
-	public JoinThread(String name) {
-		super(name);
-	}
+    public JoinThread(String name) {
+        super(name);
+    }
 
-	public void run() {
-		for (int i = 0; i < 50; i++) {
-			System.out.println(getName() + "->" + i);
-		}
-	}
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(getName() + "->" + i);
+        }
+    }
 
-	public static void main(String... args) throws InterruptedException {
-		new JoinThread("新线程").start();
+    public static void main(String... args) throws InterruptedException {
+        new JoinThread("新线程").start();
 
-		for (int i = 0; i < 50; i++) {
-			if (i == 20) {
-				JoinThread jt = new JoinThread("被Join的线程");
-				//JoinThread jt2 = new JoinThread("被Join的线程22222");
-				jt.start();
-				//jt2.start();
-				// main线程调用了jt线程的join方法，main线程必须等jt执行结束以后才会向下执行
-				jt.join();
-				//jt2.join();
-			}
-			System.out.println(Thread.currentThread().getName() + "->" + i);
-		}
-	}
+        for (int i = 0; i < 50; i++) {
+            if (i == 20) {
+                JoinThread jt = new JoinThread("被Join的线程");
+                //JoinThread jt2 = new JoinThread("被Join的线程22222");
+                jt.start();
+                //jt2.start();
+                // main线程调用了jt线程的join方法，main线程必须等jt执行结束以后才会向下执行
+                jt.join();
+                //jt2.join();
+            }
+            System.out.println(Thread.currentThread().getName() + "->" + i);
+        }
+    }
 }
