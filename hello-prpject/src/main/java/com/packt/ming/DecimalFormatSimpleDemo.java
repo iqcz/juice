d5e924f6e4d4090ff81c78ace10ce86f011067bf
Phideon <a2z0to9@outlook.com>
2018-12-27 23:35:11 +0800

    clean the code.

--- hello-prpject/src/main/java/com/packt/ming/DecimalFormatSimpleDemo.java
+++ hello-prpject/src/main/java/com/packt/ming/DecimalFormatSimpleDemo.java
@@ -4,30 +4,32 @@ import java.text.DecimalFormat;
 
 /**
  * 数字格式化
- * @author TurtusLi
+ * @author i324779
  *
  */
 public class DecimalFormatSimpleDemo {
 
     public static void main(String[] args) {
-	simpleFormat("###,###.###", 123456.789);
-	simpleFormat("00000000.###kg", 123456.789);
-	simpleFormat("000000.000", 123.78);
+        simpleFormat("###,###.###", 123456.789);
+        simpleFormat("00000000.###kg", 123456.789);
+        simpleFormat("000000.000", 123.78);
 
-	useApplePatternMethodForm("#.###%", 0.78);
-	useApplePatternMethodForm("###.##", 123456.781); // 最后一位四舍五入，保留两位小数
-	useApplePatternMethodForm("0.00\u2030", 0.789); // 千分位格式
+        useApplePatternMethodForm("#.###%", 0.78);
+        // 最后一位四舍五入，保留两位小数
+        useApplePatternMethodForm("###.##", 123456.781);
+        // 千分位格式
+        useApplePatternMethodForm("0.00\u2030", 0.789);
     }
 
     public static void simpleFormat(String pattern, double value) {
-	DecimalFormat myFormat = new DecimalFormat(pattern);
-	String output = myFormat.format(value);
-	System.out.println(value + "|" + pattern + "|" + output);
+        DecimalFormat myFormat = new DecimalFormat(pattern);
+        String output = myFormat.format(value);
+        System.out.println(value + "|" + pattern + "|" + output);
     }
 
     public static void useApplePatternMethodForm(String pattern, double value) {
-	DecimalFormat myFormat = new DecimalFormat(pattern);
-	myFormat.applyPattern(pattern);
-	System.out.println(value + "|" + pattern + "|" + myFormat.format(value));
+        DecimalFormat myFormat = new DecimalFormat(pattern);
+        myFormat.applyPattern(pattern);
+        System.out.println(value + "|" + pattern + "|" + myFormat.format(value));
     }
 }
