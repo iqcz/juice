1baa7b8fe1af0fadb9ae64acc84ce2d8a94d97cd
Phideon <a2z0to9@outlook.com>
2019-01-05 23:18:46 +0800

    去掉无用的代码。

--- hello-prpject/src/main/java/crazy/FileFilter.java
+++ hello-prpject/src/main/java/crazy/FileFilter.java
@@ -4,13 +4,14 @@ import java.io.File;
 
 public class FileFilter {
 
-	public static void main(String[] args) {
-		File file = new File(".");
-		String[] nameList = file.list((dir, name) -> name.endsWith(".java") || new File(name).isDirectory());
-		
-		for(String fileName : nameList) {
-			System.out.println(fileName);
-		}
-	}
+    public static void main(String[] args) {
+        File file = new File(".");
+        String[] nameList = file
+                .list((dir, name) -> name.endsWith(".java") || new File(name).isDirectory());
+
+        for (String fileName : nameList) {
+            System.out.println(fileName);
+        }
+    }
 
 }
