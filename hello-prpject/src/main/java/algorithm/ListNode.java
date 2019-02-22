eae0655a169ad4eca10d8823ad133e3defe16785
Phideon <a2z0to9@outlook.com>
2019-02-22 19:22:00 +0800

    逆置单向链表

---
+++ hello-prpject/src/main/java/algorithm/ListNode.java
@@ -0,0 +1,32 @@
+package algorithm;
+
+/**
+ * 单向链表
+ * @author i324779
+ */
+public class ListNode {
+
+    private int data;
+
+    private ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
