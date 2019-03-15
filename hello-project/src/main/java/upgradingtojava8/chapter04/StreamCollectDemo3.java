5ac402e34015763d6fbc6063ee881e939f8c5567
Phideon <a2z0to9@outlook.com>
2019-03-15 23:31:07 +0800

    doc: format code.

--- hello-project/src/main/java/upgradingtojava8/chapter04/StreamCollectDemo3.java
+++ hello-project/src/main/java/upgradingtojava8/chapter04/StreamCollectDemo3.java
@@ -11,37 +11,40 @@ import java.util.stream.Stream;
 public class StreamCollectDemo3 {
 
     public static void main(String[] args) {
-	
-	List<String> names = Arrays.asList("wang", "li", "zhao");
-	System.out.println(names);
-	
-	AtomicInteger counter = new AtomicInteger();
 
-	Supplier<List<String>> supplier = () -> {
-	    System.out.println("supplier called");
-	    
-	    return new ArrayList<String>() {
-		private static final long serialVersionUID = -688012119325500423L;
-		int id = counter.getAndIncrement();
+        List<String> names = Arrays.asList("wang", "li", "zhao");
+        System.out.println(names);
 
-		@Override public boolean add(String e) {
-		    System.out.println("\"add\" called for " + e + " on ArrayList " + id);
-		    return super.add(e);
-		}
+        AtomicInteger counter = new AtomicInteger();
 
-		@Override public boolean addAll(Collection<? extends String> c) {
-		    System.out.println("\"addAll\" called" + " on ArrayList " + id);
-		    return super.addAll(c);
-		}
-	    };
-	};
-	
-	String[] strings = { "a", "b", "c", "d" };
-	Stream<String> stream1 = Stream.of(strings).parallel();
-	List<String> list1 = stream1.collect(supplier, (a1, b1) -> a1.add(b1), (a2, b2) -> a2.addAll(b2));
-	
-	for (String s : list1) {
-	    System.out.println(s);
-	}
+        Supplier<List<String>> supplier = () -> {
+            System.out.println("supplier called");
+
+            return new ArrayList<>() {
+                private static final long serialVersionUID = -688012119325500423L;
+                int id = counter.getAndIncrement();
+
+                @Override
+                public boolean add(String e) {
+                    System.out.println("\"add\" called for " + e + " on ArrayList " + id);
+                    return super.add(e);
+                }
+
+                @Override
+                public boolean addAll(Collection<? extends String> c) {
+                    System.out.println("\"addAll\" called" + " on ArrayList " + id);
+                    return super.addAll(c);
+                }
+            };
+        };
+
+        String[] strings = { "a", "b", "c", "d" };
+        Stream<String> stream1 = Stream.of(strings).parallel();
+        List<String> list1 = stream1.collect(supplier, (a1, b1) -> a1.add(b1),
+                (a2, b2) -> a2.addAll(b2));
+
+        for (String s : list1) {
+            System.out.println(s);
+        }
     }
 }
