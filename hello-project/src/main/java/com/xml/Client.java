25a69972b25feffd7d8a1a7cdb507752095ecc1c
Phideon <a2z0to9@outlook.com>
2019-04-28 10:18:47 +0800

    简单xml解析。

---
+++ hello-project/src/main/java/com/xml/Client.java
@@ -0,0 +1,12 @@
+package com.xml;
+
+/**
+ * @author i324779
+ */
+public class Client {
+    public static void main(String[] args) {
+        String name = XmlUtil.getName();
+
+        System.out.println(name);
+    }
+}
---
+++ hello-project/src/main/java/com/xml/XmlUtil.java
@@ -0,0 +1,42 @@
+package com.xml;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * 解析xml配置文件
+ *
+ * @author i324779
+ */
+public class XmlUtil {
+
+    public static String getName() {
+        String name = "";
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = dbFactory.newDocumentBuilder();
+            Document document;
+
+            document = builder.parse(new File("src/main/java/com/xml/config.xml"));
+
+            NodeList nodeList = document.getElementsByTagName("name");
+            Node classNode = nodeList.item(0).getFirstChild();
+
+            name = classNode.getNodeValue().trim();
+
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+
+        return name;
+    }
+}
