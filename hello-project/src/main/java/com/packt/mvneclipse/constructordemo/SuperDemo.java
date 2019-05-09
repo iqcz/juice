6cb704df1661285a2fab8215dbf3cfacd259205f
Phideon <a2z0to9@outlook.com>
2019-05-09 23:27:33 +0800

    format code

--- hello-project/src/main/java/com/packt/mvneclipse/constructordemo/SuperDemo.java
+++ hello-project/src/main/java/com/packt/mvneclipse/constructordemo/SuperDemo.java
@@ -1,53 +1,53 @@
 package com.packt.mvneclipse.constructordemo;
 
 class Person {
-	private String name;
-	private int age;
+    private String name;
+    private int age;
 
-	public Person(String name, int age) {
-		this.setName(name);
-		this.setAge(age);
-	}
+    public Person(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
 class Student extends Person {
-	private String school;
+    private String school;
 
-	public Student(String name, int age, String school) {
-		super(name, age);
-		this.setSchool(school);
-	}
+    public Student(String name, int age, String school) {
+        super(name, age);
+        this.setSchool(school);
+    }
 
-	public String getSchool() {
-		return school;
-	}
+    public String getSchool() {
+        return school;
+    }
 
-	public void setSchool(String school) {
-		this.school = school;
-	}
+    public void setSchool(String school) {
+        this.school = school;
+    }
 }
 
 public class SuperDemo {
-	
-	public static void main(String ...args) {
-		Student s = new Student("Nick", 30, "qinghua");
-		System.out.println(s.getName() + ", " + s.getAge() + ", " + s.getSchool());
-	}
+
+    public static void main(String... args) {
+        Student s = new Student("Nick", 30, "qinghua");
+        System.out.println(s.getName() + ", " + s.getAge() + ", " + s.getSchool());
+    }
 
 }
