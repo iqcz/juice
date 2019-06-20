2f47c8f13716fc4a33fa03a1166b1e5916eeb999
Phideon <a2z0to9@outlook.com>
2019-06-20 23:40:30 +0800

    clean the code

--- hello-project/src/main/java/entity/SampleData.java
+++ hello-project/src/main/java/entity/SampleData.java
@@ -1,10 +1,10 @@
 package entity;
 
+import static java.util.Arrays.asList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
-
-import static java.util.Arrays.asList;
 
 public class SampleData {
 
@@ -14,25 +14,25 @@ public class SampleData {
     public static final Artist georgeHarrison = new Artist("George Harrison", "UK");
     public static final Artist ringoStarr = new Artist("Ringo Starr", "UK");
 
-    public static final List<Artist> membersOfTheBeatles = 
-	    Arrays.asList(johnLennon, paulMcCartney, georgeHarrison, ringoStarr);
+    public static final List<Artist> membersOfTheBeatles =
+            Arrays.asList(johnLennon, paulMcCartney, georgeHarrison, ringoStarr);
 
     // have members' artists
     public static final Artist theBeatles = new Artist("The Beatles", membersOfTheBeatles, "UK");
 
-    public static final Album aLoveSupreme = new Album("A Love Supreme", 
-	    asList(new Track("Acknowledgement", 467), new Track("Resolution", 442)), 
-	    asList(johnColtrane));
+    public static final Album aLoveSupreme = new Album("A Love Supreme",
+            asList(new Track("Acknowledgement", 467), new Track("Resolution", 442)),
+            asList(johnColtrane));
 
-    public static final Album sampleShortAlbum = new Album("sample Short Album", 
-	    asList(new Track("short track", 30)),
-	    asList(johnColtrane));
+    public static final Album sampleShortAlbum = new Album("sample Short Album",
+            asList(new Track("short track", 30)),
+            asList(johnColtrane));
 
-    public static final Album manyTrackAlbum = new Album("sample Short Album", 
-	    asList(new Track("short track", 30), new Track("short track 2", 30), 
-		    new Track("short track 3", 30), new Track("short track 4", 30), 
-		    new Track("short track 5", 30)),
-	    asList(johnColtrane));
+    public static final Album manyTrackAlbum = new Album("sample Short Album",
+            asList(new Track("short track", 30), new Track("short track 2", 30),
+                    new Track("short track 3", 30), new Track("short track 4", 30),
+                    new Track("short track 5", 30)),
+            asList(johnColtrane));
 
     public static Stream<Album> albums = Stream.of(aLoveSupreme);
 
